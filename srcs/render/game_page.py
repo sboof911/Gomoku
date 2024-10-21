71cbd51a1dee15c9a13e6b0d9f3df729f596fbf5
Sboof <amaach@student.1337.ma>
2024-10-21 18:23:03 +0100

    game page render almost finish

---
+++ srcs/render/game_page.py
@@ -0,0 +1,117 @@
+from tkinter import Button, PhotoImage, Frame, Label, Canvas
+from srcs.render.render_init import render
+
+
+def create_text_players(current_render : render, mode):
+    player1 = "Player 1" if mode == "players" else "Player"
+    current_render.canvas.create_text(
+        10.0,
+        90.0,
+        anchor="nw",
+        text=player1,
+        fill="#1E1E1E",
+        font=("Jaini Regular", 30 * -1)
+    )
+
+    player2 = "Player 2" if mode == "players" else "AI"
+    current_render.canvas.create_text(
+        487.0,
+        79.0,
+        anchor="nw",
+        text=player2,
+        fill="#FCF5F5",
+        font=("Jaini Regular", 30 * -1)
+    )
+
+def board_click(event):
+    x, y = event.x, event.y
+    print(f"Clicked at: (x={x}, y={y})")
+
+def create_grid(canvas : Canvas, width, height, rows, cols):
+    # Calculate the size of each cell in the grid
+    x = 14
+    y = 14
+    width = width - (x * 2)
+    height = height - (y * 2)
+    cell_width = width / cols
+    cell_height = height / rows
+
+    # Create the grid by drawing vertical and horizontal lines
+    for i in range(rows + 1):
+        canvas.create_line(x, y + i * cell_height, x + width, y + i * cell_height, fill="black")
+    for j in range(cols + 1):
+        canvas.create_line(x + j * cell_width, y, x + j * cell_width, y + height, fill="black")
+
+def board_game(current_render : render):
+    kwargs = dict(x=125.0, y=75.0, width=350.0, height=350.0)
+
+    board_game_img = PhotoImage(file=current_render.get_image("game_page", "board_game"))
+    # Create a frame for the board game
+    frame = Frame(current_render.window, borderwidth=0, highlightthickness=0, relief="flat")
+    frame.pack()
+    frame.place(**kwargs)
+
+    # Create a canvas for drawing the grid
+    canvas = Canvas(frame, width=kwargs['width'], height=kwargs['height'], bg='white', highlightthickness=0)
+    canvas.place(x=0, y=0)  # Place the canvas at the top left of the frame
+
+    # Create a label for the background image
+    canvas.create_image(
+            kwargs["width"] / 2,
+            kwargs["height"] / 2,
+            image=board_game_img)
+
+    # Create a canvas for drawing the grid
+    # canvas = Canvas(frame, width=kwargs['width'], height=kwargs['height'], bg='white', highlightthickness=0)
+    # canvas.place(x=0, y=0)  # Place the canvas at the top left of the frame
+
+    # Create the grid on the canvas
+    create_grid(canvas, rows=20, cols=20, width=kwargs["width"], height=kwargs["height"])
+
+    # Bind the click event to the frame
+    canvas.bind("<Button-1>", board_click)
+    
+    current_render.save.append(board_game_img)
+    # current_render.save.append(frame)
+
+def back_button(current_render : render):
+    from srcs.render.main_page import render_main_page
+    button_image_2 = PhotoImage(
+        file=current_render.get_image("game_page", "back"))
+    button_2 = Button(
+        image=button_image_2,
+        borderwidth=0,
+        highlightthickness=0,
+        command=lambda: render_main_page(current_render),
+        relief="flat"
+    )
+    button_2.place(
+        x=12.0,
+        y=11.0,
+        width=43.0,
+        height=44.0
+    )
+
+    button_image_hover_2 = PhotoImage(
+        file=current_render.get_image("game_page", "hover_back"))
+
+    def button_2_hover(e):
+        button_2.config(
+            image=button_image_hover_2
+        )
+    def button_2_leave(e):
+        button_2.config(
+            image=button_image_2
+        )
+
+    button_2.bind('<Enter>', button_2_hover)
+    button_2.bind('<Leave>', button_2_leave)
+
+def render_game_page(current_render : render, mode):
+    current_render.clear_window()
+    current_render.set_canvas()
+    current_render.canvas.place(x = 0, y = 0)
+    current_render.set_backgroud()
+    create_text_players(current_render, mode)
+    board_game(current_render)
+    back_button(current_render)
--- srcs/render/render_init.py
+++ srcs/render/render_init.py
@@ -5,6 +5,7 @@ INIT_HEIGHT = 500
 
 class ResizingCanvas(tk.Canvas):
     def __init__(self,parent,**kwargs):
+        self.save = []
         tk.Canvas.__init__(self,parent,**kwargs)
         self.bind("<Configure>", self.on_resize)
         self.height = self.winfo_reqheight()
@@ -31,24 +32,44 @@ class render:
 
     def get_image(self, page_name, image_name):
         current_folder = os.path.dirname(os.path.abspath(__file__))
-        return f"{current_folder}/srcs/{page_name}/{image_name}_0.png"
+        return f"{current_folder}/srcs/{page_name}/{image_name}_0.png" # TODO : change the 0 to the current settings
 
     def get_window_size(self):
         width = self._window.winfo_width() if self._window.winfo_width() > 1 else INIT_WIDTH
         height = self._window.winfo_height() if self._window.winfo_height() > 1 else INIT_HEIGHT
+
         return width, height
 
-    def set_canvas(self, frame):
-        width, height = self.get_window_size()
-        self._canvas = ResizingCanvas(
-            frame,
+    def clear_window(self):
+        self.save = []
+        for widget in self._window.winfo_children():
+            widget.pack_forget()
+
+    def set_canvas(self, frame = None):
+        window = self._window if frame is None else frame
+        width, height = self.get_window_size() if frame is None else (window.winfo_width(), window.winfo_height())
+        canvas = ResizingCanvas(
+            window,
             bg = "#FFFFFF",
             height = height,
             width = width,
             bd = 0,
             highlightthickness = 0,
         )
-        self._canvas.pack(fill=tk.BOTH, expand=tk.YES)
+        if frame is None:
+            self._canvas = canvas
+            self._canvas.pack(fill=tk.BOTH, expand=tk.YES)
+        else:
+            return canvas
+
+    def set_backgroud(self):
+        self._backgroud_img = tk.PhotoImage(
+            file=self.get_image("backgrouds", "backgroud")) # need to get the backgroud from the settings
+        self._canvas.create_image(
+            300.0,
+            250.0,
+            image=self._backgroud_img
+        )
 
     @property
     def window(self):
