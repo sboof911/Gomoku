3ad5272cc6c550fd4bbf40f5ae1eb899cfee22ce
Sboof <amaach@student.1337.ma>
2024-11-22 15:45:04 +0100

    fixed

--- srcs/backend/ai/Minimax.py
+++ srcs/backend/ai/Minimax.py
@@ -1,34 +1,26 @@
-from srcs.backend.ai.heuristic_evaluation import heuristic_evaluation, MAX_SCORE
+from srcs.backend.ai.heuristic_evaluation import heuristic_evaluation, check_index, MAX_SCORE
 
-def get_best_available_actions(board, ZERO, board_array, x1=-2, y1=-2, connect_num=2):
+def get_best_available_actions(board_array, used_actions, ZERO, x1=-2, y1=-2):
     available_actions = []
-    directions = ["Horizontal", "Vertical", "Normal_Diag", "Reversed_Diag"]
-    for x in range(board._size):
-        for y in range(board._size):
-            if board_array[y][x] == ZERO:
-                adjucents = board.get_Adjucents(x, y, connect_num)
-                found = False
-                adj_to_last_play = False
-                for direction in directions:
-                    for x0, y0 in adjucents[direction]:
-                        if x0 is not None and y0 is not None:
-                            if board_array[y0][x0] != ZERO:
-                                if x0==x1 and y0==y1:
-                                    adj_to_last_play = True
-                                found = True
-                                break
-                    if found:
-                        if (x, y) not in available_actions:
-                            if adj_to_last_play:
-                                available_actions.insert(0, (x, y))
-                            available_actions.append((x, y))
-                        break
+    directions = [(1, 1), (0, 1), (1, 0), (1, -1),
+                  (0, -1), (-1, -1), (-1, 0), (-1, 1)]
+    for x, y in used_actions:
+        for dx, dy in directions:
+            x0 = x + dx
+            y0 = y + dy
+            if check_index(board_array.shape[0], x0, y0):
+                if board_array[y0][x0] == ZERO:
+                    if (x0, y0) not in available_actions:
+                        if x == x1 and y == y1:
+                            available_actions.insert(0, (x0, y0))
+                        else:
+                            available_actions.append((x0, y0))
 
     return available_actions
 
 def minimax(board, board_array, depth, players, ai_player_index,
             x_value=-2, y_value=-2, maximizing_player=True,
-            alpha=float('-inf'), beta=float('inf'), available_actions=None):
+            alpha=float('-inf'), beta=float('inf'), used_actions={}):
 
     score = heuristic_evaluation(board_array, players, ai_player_index, board._connect_num)
 
@@ -37,9 +29,8 @@ def minimax(board, board_array, depth, players, ai_player_index,
             return 0, None, None
         return score, None, None
 
-    if not available_actions:
-        available_actions = get_best_available_actions(
-            board, players[0].ZERO, board_array, x_value, y_value)
+    available_actions = get_best_available_actions(
+        board_array, used_actions, players[0].ZERO, x_value, y_value)
 
     opponent_player_index = (ai_player_index + 1) % 2
     max_eval = float('-inf')
@@ -56,15 +47,20 @@ def minimax(board, board_array, depth, players, ai_player_index,
             played, board_array, captured_stones_pos = board.place_stone(
                 x, y, players, opponent_player_index, board_array)
         if played:
+            used_actions.add((x, y))
             eval, _, _ = minimax(
                 board, board_array, depth-1, players,
-                ai_player_index, x, y, not maximizing_player, alpha, beta)
+                ai_player_index, x, y, not maximizing_player,
+                alpha, beta, used_actions)
 
             for x0, y0 in captured_stones_pos[0]:
                 board_array[y0][x0] = players[0].BLACK
+                used_actions.add((x0, y0))
             for x0, y0 in captured_stones_pos[1]:
                 board_array[y0][x0] = players[0].WHITE
+                used_actions.add((x0, y0))
             board_array[y][x] = players[0].ZERO
+            used_actions.remove((x, y))
             players[0].peer_captured = player1_peer_capture
             players[1].peer_captured = player2_peer_capture
 
--- srcs/backend/ai/ai_manager.py
+++ srcs/backend/ai/ai_manager.py
@@ -30,54 +30,18 @@ class AI_manager():
         current_time = time()
 
         # x, y = self.launch_threads(current_player_index)
-        available_actions = self.get_available_actions()
-        if len(available_actions) == 0:
+        if len(board._used_actions) == 0:
             center = self._board._size//2
             return center, center
 
         _, x, y = minimax(board, board._board, self._depth, players, current_player_index,
-                          self._board.last_play[0], self._board.last_play[1], available_actions=available_actions)
+                          self._board.last_play[0], self._board.last_play[1], used_actions=board._used_actions)
 
         if self._debug_mode:
             print("Can't print the time, debug mode is on")
         else:
             print(f"Time to get best move:{time()-current_time:.2f}s")
         return x, y
-
-    def get_available_actions(self):
-        used_actions = self._board.get_used_actions(self._board._board)
-        if not used_actions:
-            return []
-
-        available_actions = []
-        directions = ["Horizontal", "Vertical", "Normal_Diag", "Reversed_Diag"]
-        x0, y0 = self._board.last_play
-        available_actions_last_play = []
-        for x, y in used_actions:
-            adjucents = self._board.get_Adjucents(x, y, 2)
-            for key in range(len(adjucents[directions[0]])):
-                for direction in directions:
-                    x1 = adjucents[direction][key][0]
-                    y1 = adjucents[direction][key][1]
-                    if x1 is not None and y1 is not None:
-                        if self._board._board[y1][x1] == self.ZERO:
-                            if (x1, y1) not in available_actions:
-                                if x1 == x0 and y1 == y0:
-                                    # available_actions_last_play.insert(0, (x1, y1))
-                                    available_actions.insert(0, (x1, y1))
-                                else:
-                                    available_actions.append((x1, y1))
-
-        # start = 0
-        # actions_per_thread = len(available_actions) // self._thread_num
-        # for x, y in available_actions_last_play:
-        #     available_actions.remove((x, y))
-        #     available_actions.insert(start, (x, y))
-        #     start = start + actions_per_thread
-        #     if start >= len(available_actions):
-        #         start = 0
-
-        return available_actions
 
     def launch_threads(self, current_player_index):
         queue_list = Queue()
